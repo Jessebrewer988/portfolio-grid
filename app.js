d83ccb0984f2cccc465107d8835323e56c6d01ef
Jesse Brewer <jbrewerwriter@gmail.com>
2019-05-14 18:14:47 -0400

    added a project

---
+++ app.js
@@ -0,0 +1,10 @@
+const goToSite = site => {
+  switch (site) {
+    case 'dq':
+      window.location.href =
+        'https://jessebrewer988.github.io/Dragon-Quest-NES-Soundboard/';
+      break;
+    default:
+      null;
+  }
+};
